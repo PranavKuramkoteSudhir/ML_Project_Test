6967f268b43594a7358b114094d7901ed367ee95
PranavKuramkotesudhir <pranavkuramkotesudhir@gmail.com>
2024-03-11 19:18:24 -0400

    exception.py update

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -20,7 +20,7 @@ class CustomException(Exception):
 
 if __name__ == '__main__':
     try:
-        1/0
+        a= 1/0
     except Exception as e:
         logging.info('Trying to divide by zero')
         raise CustomException(e,sys)
